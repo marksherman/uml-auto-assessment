41c302f86e34f8c51dddbfa6475110257593b112
Mark Sherman <msherman@cs.uml.edu>
2012-07-25 16:52:13 -0400

    Added CppTddPlugin as COutputComparisonPlugin, to be converted
    Using the CppTddPlugin as a base for making our custom plugin.
    Currently, it is CppTdd unchanged except for the directory  name.

---
+++ COutputComparisonPlugin/tests/__instructor__Test.h
@@ -0,0 +1,18 @@
+#include <cxxtest/TestSuite.h>
+
+
+class __instructor__Test :
+    public CxxTest::TestSuite
+{
+public:
+    void setUp()
+    {
+    }
+    void tearDown()
+    {
+    }
+
+    void testBlank()
+    {
+    }
+};
