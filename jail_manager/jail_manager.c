b86071eebd59618ae921a7fd5dc27760be145b0a
Mark Sherman <msherman@cs.uml.edu>
2012-08-31 16:27:04 -0400

    Setup main thread to receive on pipe. Added comment header and such.

--- jail_manager/jail_manager.c
+++ jail_manager/jail_manager.c
@@ -1,3 +1,18 @@
+/*****************************************************************************
+* jail_manager.c
+* Main executable that provides a secure jail for student code to be run in.    
+* THIS PROGRAM RUNS WITH ROOT PRIVILEDGES. This program forks a subp which 
+* exec's the python code to do the real plugin work, establishing 
+* communication with that script via a pipe. When the python script signals, 
+* this program will create a chroot jail and execute the student code as a 
+* non-priv user. When the student code returns, the chroot ends, the python 
+* script is signalled, and this program waits for the python script to 
+* complete. All of this creates a secure environment for student code.  
+*
+* by Mark Sherman, msherman@cs.uml.edu
+* Tracked at github.com/marksherman/uml-auto-assessment
+******************************************************************************/   
+
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -46,9 +61,10 @@ int main( void ) {
     
     if( pid == (pid_t) 0){
         /* This is the child process. */
+        /* In here we will exec the python script to do the real work*/
         
-        close( mypipe[1] ); /* close the WRITE end of the pipe */
-        read_from_pipe( mypipe[0] );
+        close( mypipe[0] ); /* closes READ end of pipe */
+        write_to_pipe( mypipe[1] );
         return EXIT_SUCCESS;
         
     } else if(pid < (pid_t) 0){
@@ -59,9 +75,12 @@ int main( void ) {
         
     } else {
         /* This is the parent process. */
+        /* In here we will wait for the python script to signal,
+         * and then create the chroot jail and execute as desired.
+         */
         
-        close( mypipe[0] ); /* closes READ end of pipe */
-        write_to_pipe( mypipe[1] ); 
+        close( mypipe[1] ); /* close the WRITE end of the pipe */
+        read_from_pipe( mypipe[0] );
 
         return EXIT_SUCCESS;
     }
