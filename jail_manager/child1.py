2f15eeab168b965b6dd907f3ef8bb2c2419497d5
Mark Sherman <msherman@cs.uml.edu>
2012-08-31 17:42:08 -0400

    child1.py MIGHT be writing to the pipe. PYCHECKER WILL KILL THIS
    I think pychecker has an issue with the system pipes. It chokes,
    replacing the *actual code file* with just "Sent from python" which of
    course isn't valid code. F'in weird.

--- jail_manager/child1.py
+++ jail_manager/child1.py
@@ -1,5 +1,13 @@
 #! /usr/bin/env python
 
 import sys
+import pipes
 
 print "Running python! Argument: " + sys.argv[1]
+
+t = pipes.Template()
+pipe_out = t.open( sys.argv[1] , 'w' )
+
+pipe_out.write('Sent from python!\n')
+
+pipe_out.close()
