df93f0c21e67dea8e3e08068e85d6edbb869271a
Mark Sherman <msherman@webcat.cs.uml.edu>
2012-09-04 13:20:43 -0400

    Added header file for sockets. Mostly a test commit from the git repo on the server.

--- jail_manager/jail_manager.c
+++ jail_manager/jail_manager.c
@@ -14,6 +14,7 @@
 ******************************************************************************/   
 
 #include <sys/types.h>
+#include <sys/socket.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
